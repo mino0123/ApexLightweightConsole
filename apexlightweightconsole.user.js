5e30f6e806fb0e8024cd70daaa149241df98e831
mino0123 <minoaw@gmail.com>
2013-10-22 22:33:28 +0900

    未使用のCSI API接続関数を削除

--- apexlightweightconsole.user.js
+++ apexlightweightconsole.user.js
@@ -415,39 +415,6 @@ ApexCSIAPI.createGeneralSuccessListener = function (callback) {
 ApexCSIAPI.generalFailureListener = function () {
     console.log(arguments);
 };
-ApexCSIAPI.prototype.config = function (callback) {
-    unsafeWindow.Ext.Ajax.request({
-        interval : 2E4,
-        url      : this.url,
-        params   : {action : 'CONFIG'},
-        success  : ApexCSIAPI.createGeneralSuccessListener(callback),
-        failure : ApexCSIAPI.generalFailureListener
-    });
-};
-ApexCSIAPI.prototype.getAlreadyFetched = function () {
-    return (this.traces || []).map(function (trace) {
-        return trace.id;
-    });
-};
-ApexCSIAPI.prototype.executeAnonymous = function (anonymousBody, callback) {
-    var params = {
-        action          : 'EXEC',
-        alreadyFetched  : this.getAlreadyFetched(),
-        anonymousBody   : anonymousBody,
-        startDate       : Date.now(),
-        fewmetLocations : JSON.stringify([]),
-        openEditors     : '',
-        traceLevels     : JSON.stringify({"APEX_CODE":"FINEST","VALIDATION":"INFO","WORKFLOW":"INFO","APEX_PROFILING":"INFO","DB":"INFO","CALLOUT":"INFO","VISUALFORCE":"INFO","SYSTEM":"DEBUG"}),
-        workspace       : JSON.stringify([])
-    };
-    return unsafeWindow.Ext.Ajax.request({
-        interval : 2E4,
-        url      : this.url,
-        params   : params,
-        success  : ApexCSIAPI.createGeneralSuccessListener(callback),
-        failure  : ApexCSIAPI.generalFailureListener
-    });
-};
 ApexCSIAPI.prototype.open = function (entity, callback) {
     var workspace = [];
     workspace.push({
